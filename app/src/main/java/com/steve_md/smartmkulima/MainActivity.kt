237d8163c500c0482551bcbce6f9328257d924de
MuindiStephen <stephenmuindi241@gmail.com>
2022-10-31 12:26:04 +0300

    initial

---
+++ app/src/main/java/com/steve_md/smartmkulima/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.steve_md.smartmkulima
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
