3115a44bb69acc9aced2f3e727418f83c306b5ed
MuindiStephen <stephenmuindi241@gmail.com>
2022-11-16 01:34:29 +0300

    worked on signWithEmail

--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignInDetailsWithEmailFragment.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignInDetailsWithEmailFragment.kt
@@ -1,24 +1,109 @@
 package com.steve_md.smartmkulima.ui.fragments.auth
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.steve_md.smartmkulima.R
-import com.steve_md.smartmkulima.databinding.FragmentRecoverPasswordWithEmailBinding
+import com.steve_md.smartmkulima.databinding.FragmentSignInDetailsWithEmailBinding
+import com.steve_md.smartmkulima.utils.Resource
+import com.steve_md.smartmkulima.utils.toast
+import com.steve_md.smartmkulima.viewmodel.AuthenticationViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 
 class SignInDetailsWithEmailFragment : Fragment() {
 
-    private lateinit var binding: FragmentRecoverPasswordWithEmailBinding
+    // According to the docs this is a nice way get binding
+    private var _binding: FragmentSignInDetailsWithEmailBinding? = null
+    private val binding get() = _binding!!
 
-override  fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+    // view model
+    private val loginWithEmailViewModel : AuthenticationViewModel by viewModels()
+
+   override  fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in_details_with_email, container, false)
+        _binding = FragmentSignInDetailsWithEmailBinding.inflate(layoutInflater, container, false)
+       return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signInWithPhoneInsteadText.setOnClickListener {
+            navigateToSignInWithPhoneFragment()
+        }
+
+        binding.signInWithEmailButton.setOnClickListener {
+            if (isValidCredentials()) loginUser()
+            else toast("Enter valid credentials")
+        }
+
+        lifecycleScope.launchWhenResumed {
+            loginWithEmailViewModel.loginResult.collectLatest {
+                when (it) {
+                    Resource.Loading -> {
+                        toast("Loading")
+                    }
+                    is Resource.Error -> {
+                        toast("Couldn't log in")
+                    }
+                    is Resource.Success -> {
+                        val userId = it.value.email
+
+                        // check whether user data is null or available in the backend db api
+                        it.value.email.let {
+
+                            // if available then login the user successfully.
+                            if (it !=null) {
+                                toast("Successfully Logged In")
+                                navigateToHomeDashboardFragment()
+                            } else {
+                                toast("Account Doesn't exist")
+                            }
+                        }
+
+                    }
+                    null -> {}
+                }
+            }
+        }
+
+    }
+
+    private fun navigateToHomeDashboardFragment() {
+        findNavController().navigate(R.id.action_signInDetailsWithEmailFragment_to_homeActivity2)
+    }
+
+    private fun loginUser() {
+        Log.i("VAVAVA","VIEWMODEL IS nUll -> ${loginWithEmailViewModel == null}")
+        loginWithEmailViewModel.loginUserUsingEmail(
+            binding.inputLoginEmail.text.toString(),
+            binding.inputLoginPassword.text.toString()
+        )
+    }
+
+    private fun isValidCredentials(): Boolean {
+        return binding.inputLoginEmail.text.isNullOrEmpty().not().also {
+            if (!it) binding.enterLoginEmailAddress.error = "Invalid Email"
+        } && binding.inputLoginPassword.text.isNullOrEmpty().not().also {
+            if (!it) binding.enterLoginPassword.error = "Invalid Password"
+        }
+    }
+
+    private fun navigateToSignInWithPhoneFragment() {
+        findNavController().navigate(R.id.action_signInDetailsWithEmailFragment_to_signInDetailsFragment)
+    }
+
+
 
 }
--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignUpDetailsWithEmailFragment.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignUpDetailsWithEmailFragment.kt
@@ -5,29 +5,105 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.steve_md.smartmkulima.R
 import com.steve_md.smartmkulima.databinding.FragmentSignUpDetailsWithEmailBinding
+import com.steve_md.smartmkulima.utils.Resource
+import com.steve_md.smartmkulima.utils.toast
+import com.steve_md.smartmkulima.viewmodel.AuthenticationViewModel
+import kotlinx.coroutines.flow.collectLatest
 
 
 class SignUpDetailsWithEmailFragment : Fragment() {
-    private lateinit var binding: FragmentSignUpDetailsWithEmailBinding
+
+    // View binding
+    private var _binding: FragmentSignUpDetailsWithEmailBinding? = null
+    private val binding get() = _binding!!
+
+    // View model
+    private val signUpWithEmailViewModel : AuthenticationViewModel by viewModels()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        binding = FragmentSignUpDetailsWithEmailBinding.inflate(layoutInflater, container, false)
-
-        binding.signUpWithPhoneInsteadText.setOnClickListener {
-            findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_signUpDetailsMainFragment)
-        }
-        binding.buttonSignUpWithEmail.setOnClickListener {
-            findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_emailVerificationFragment)
-        }
+        _binding = FragmentSignUpDetailsWithEmailBinding.inflate(layoutInflater, container, false)
 
         return binding.root
     }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.signUpWithPhoneInsteadText.setOnClickListener {
+            navigateToPhoneFragment()
+        }
+
+
+        binding.buttonSignUpWithEmail.setOnClickListener {
+//            findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_emailVerificationFragment)
+            if (isValidRegistrationDetails()) registerUser()
+                else toast("Unable to register. Invalid credentials!")
+        }
+
+        lifecycleScope.launchWhenResumed {
+           signUpWithEmailViewModel.registerResult.collectLatest {
+               when (it) {
+                   is Resource.Success -> {
+                       toast("Registered Successfully, Please Login")
+                       navigateToEmailVerificationFragment()
+                   }
+
+                   is Resource.Error -> {
+                       toast("Couldn't register account!")
+                   }
+
+                   is Resource.Loading -> {
+
+                   }
+                   null -> {}
+
+               }
+           }
+        }
+
+
+    }
+
+    private fun navigateToEmailVerificationFragment() {
+        findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_emailVerificationFragment)
+    }
+
+    private fun registerUser() {
+        signUpWithEmailViewModel.registerUsingEmail(
+            binding.inputUserName.text.toString(),
+            binding.inputEmailAddress.text.toString(),
+            binding.inputPassword.text.toString(),
+            binding.inputConfirmPassword.text.toString()
+        )
+    }
+
+    private fun isValidRegistrationDetails(): Boolean {
+        return binding.inputUserName.text.isNullOrEmpty().not().also {
+            if (!it) binding.enterUserName.error = "Invalid email"
+        } && binding.inputEmailAddress.text.isNullOrEmpty().not().also {
+            if (!it) binding.enterEmailAddress.error = "Invalid Name"
+        } && binding.inputPassword.text.isNullOrEmpty().not().also {
+            if (!it) binding.enterPassword.error = "Invalid Password"
+        } && binding.inputConfirmPassword.text.isNullOrEmpty().not().also {
+            if (!it) binding.confirmPassword.error = "Invalid Password"
+        }
+    }
+
+    private fun navigateToPhoneFragment() {
+        findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_signUpDetailsMainFragment)
+    }
+
 
 }
