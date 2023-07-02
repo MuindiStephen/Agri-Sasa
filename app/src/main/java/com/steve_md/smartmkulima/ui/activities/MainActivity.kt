47afeae8a78bdd7d985001db6e94007a79c8183e
MuindiStephen <stephenmuindi241@gmail.com>
2023-07-02 14:44:49 +0300

    updates and code refactoring to navigation: MainActivity

--- app/src/main/java/com/steve_md/smartmkulima/ui/activities/MainActivity.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/activities/MainActivity.kt
@@ -5,10 +5,12 @@ package com.steve_md.smartmkulima.ui.activities
 * */
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.steve_md.smartmkulima.R
 import com.steve_md.smartmkulima.databinding.ActivityMainBinding
 
@@ -20,13 +22,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        supportActionBar?.hide()
         
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
                 as NavHostFragment
         navController = navHostFragment.findNavController()
 
-        // setupActionBarWithNavController(navController)
+        binding.bottomNavigationView.apply {
+            setupWithNavController(navController)
+        }
+        navController.addOnDestinationChangedListener {_,destination,_ ->
+           when(destination.id) {
+               R.id.homeDashboardFragment2 -> {
+                   binding.bottomNavigationView.visibility = View.VISIBLE
+               }
+               R.id.applyInsuranceFragment -> {
+                   binding.bottomNavigationView.visibility = View.VISIBLE
+               }
+               R.id.newFarmingTechnologyFragment -> {
+                   binding.bottomNavigationView.visibility = View.VISIBLE
+               }
+               R.id.hireFarmEquipmentsFragment -> {
+                   binding.bottomNavigationView.visibility = View.VISIBLE
+               }
+               else -> {
+                   binding.bottomNavigationView.visibility = View.INVISIBLE
+               }
+           }
+        }
 
     }
 }
