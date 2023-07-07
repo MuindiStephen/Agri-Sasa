a787612bfeb1b82654770c7c28c4f52bdef1a6c1
MuindiStephen <stephenmuindi241@gmail.com>
2023-07-07 14:22:10 +0300

    generated all api returns

---
+++ app/src/main/java/com/steve_md/smartmkulima/data/dto/EquipmenttsResponseItem.kt
@@ -0,0 +1,17 @@
+package com.steve_md.smartmkulima.data.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class EquipmenttsResponseItem(
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("imageUrl")
+    val imageUrl: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("priceHire")
+    val priceHire: String,
+    @SerializedName("year")
+    val year: String
+)
