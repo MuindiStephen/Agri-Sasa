3ab0f404be6c120772e33958bf75002511500673
MuindiStephen <stephenmuindi241@gmail.com>
2023-07-25 14:20:58 +0300

    added settings

--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/YourAccountFragment.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/YourAccountFragment.kt
@@ -1,5 +1,6 @@
 package com.steve_md.smartmkulima.ui.fragments.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,14 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.steve_md.smartmkulima.databinding.FragmentYourAccountBinding
+import com.steve_md.smartmkulima.ui.fragments.others.Settings
 
 
 class YourAccountFragment : Fragment() {
 
     private var _binding:FragmentYourAccountBinding? = null
     private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,5 +27,16 @@ class YourAccountFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.hide()
+
+        initBinding()
+    }
+
+    private fun initBinding() {
+        binding.apply {
+            settings.setOnClickListener {
+               startActivity(Intent(requireActivity().applicationContext, Settings::class.java))
+                requireActivity().finish()
+            }
+        }
     }
 }
