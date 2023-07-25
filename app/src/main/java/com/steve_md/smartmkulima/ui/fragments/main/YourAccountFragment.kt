7ce101ddcb91d86b26d7442eb8efb7537cc019f7
MuindiStephen <stephenmuindi241@gmail.com>
2023-07-25 13:35:07 +0300

    profile screen design

---
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/YourAccountFragment.kt
@@ -0,0 +1,28 @@
+package com.steve_md.smartmkulima.ui.fragments.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.steve_md.smartmkulima.databinding.FragmentYourAccountBinding
+
+
+class YourAccountFragment : Fragment() {
+
+    private var _binding:FragmentYourAccountBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentYourAccountBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).supportActionBar?.hide()
+    }
+}
