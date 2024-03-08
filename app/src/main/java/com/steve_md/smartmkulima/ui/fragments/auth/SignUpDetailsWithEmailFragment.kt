741fe2918e3fa30f77151841cffb9bcb26c60605
MuindiStephen <stephenmuindi241@gmail.com>
2024-03-08 14:39:15 +0300

    [signup fragment] code refactor

--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignUpDetailsWithEmailFragment.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/auth/SignUpDetailsWithEmailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavController
@@ -18,7 +19,12 @@ import com.google.firebase.database.FirebaseDatabase
 import com.steve_md.smartmkulima.R
 import com.steve_md.smartmkulima.databinding.FragmentSignUpDetailsWithEmailBinding
 import com.steve_md.smartmkulima.model.User
+import com.steve_md.smartmkulima.utils.EventObserver
 import com.steve_md.smartmkulima.utils.displaySnackBar
+import com.steve_md.smartmkulima.utils.hideKeyboard
+import com.steve_md.smartmkulima.utils.snackBar
+import com.steve_md.smartmkulima.utils.toast
+import com.steve_md.smartmkulima.viewmodel.AuthViewModel
 import com.steve_md.smartmkulima.viewmodel.AuthenticationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
@@ -27,17 +33,10 @@ import timber.log.Timber
 @AndroidEntryPoint
 class SignUpDetailsWithEmailFragment : Fragment() {
 
-    // View binding
     private var _binding: FragmentSignUpDetailsWithEmailBinding? = null
     private val binding get() = _binding!!
 
-    // View model
-    private val signUpWithEmailViewModel: AuthenticationViewModel by viewModels()
-
-
-    // firebase
-    private lateinit var databaseReference: DatabaseReference
-    private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel: AuthViewModel by viewModels()
 
     private lateinit var navController: NavController
 
@@ -46,7 +45,6 @@ class SignUpDetailsWithEmailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         _binding = FragmentSignUpDetailsWithEmailBinding.inflate(layoutInflater, container, false)
 
         return binding.root
@@ -64,53 +62,17 @@ class SignUpDetailsWithEmailFragment : Fragment() {
         binding.mainAuthsToolbar.title = null
 
 
-        // Initialize firebase
-        databaseReference =  FirebaseDatabase.getInstance().getReference("users")
-        firebaseAuth = FirebaseAuth.getInstance()
+        subscribeToObservers()
 
 
         binding.buttonSignUpWithEmail.setOnClickListener {
-            val username: String = binding.inputUserName.text.toString()
-            val email: String = binding.inputEmailAddress.text.toString()
-            val password: String = binding.inputPassword.text.toString()
-            val confirmPassword: String = binding.inputConfirmPassword.text.toString()
-
-            if (username.isEmpty()) {
-                binding.inputUserName.error = "Empty Username"
-            }
-            else if (email.isEmpty()) {
-                binding.inputEmailAddress.error = ("Empty Email or invalid")
-            }
-            else if (password.isEmpty() || password.length<8) {
-                binding.inputPassword.error = ("Password too weak or password empty")
-            }
-            else if (confirmPassword.isEmpty() || confirmPassword.length<8) {
-                binding.inputConfirmPassword.error = ("Password too weak or password empty")
-            }
-            else {
-                binding.progressBarSignUp.visibility = View.VISIBLE
-
-                firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task->
-
-                    if (task.isSuccessful) {
-                        val userId : String  = firebaseAuth.currentUser!!.uid
-                        val user = User(username, email, password, confirmPassword)
-                        databaseReference.child(userId).setValue(user)
-
-                        Timber.i("User account created: $user")
-                        displaySnackBar("Account created successfully")
-
-                        firebaseAuth.signOut()
-
-                        binding.progressBarSignUp.visibility = View.INVISIBLE
-
-                        navigateToEmailVerificationFragment()
-                    } else {
-                        binding.progressBarSignUp.visibility = View.INVISIBLE
-                        Toast.makeText(requireContext(),task.exception?.localizedMessage,Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
+            viewModel.registerUser(
+                binding.inputUserName.text.toString(),
+                binding.inputEmailAddress.text.toString(),
+                binding.inputConfirmPassword.text.toString(),
+                binding.inputPassword.text.toString()
+            )
+            hideKeyboard()
         }
 
         binding.alreadyAcc.setOnClickListener {
@@ -119,45 +81,19 @@ class SignUpDetailsWithEmailFragment : Fragment() {
         binding.signUpWithPhoneInsteadText.setOnClickListener {
             navigateToPhoneFragment()
         }
-        /**
-        binding.buttonSignUpWithEmail.setOnClickListener {
-            if (isValidRegistrationDetails()) {
-                val direction =
-                    SignUpDetailsWithEmailFragmentDirections.actionSignUpDetailsWithEmailFragmentToEmailVerificationFragment(
-                        binding.inputEmailAddress.text.toString()
-                    )
-                findNavController().navigate(
-                    direction
-                )
-                registerUser()
+    }
 
-            } else displaySnackBar("Unable to register. Empty Strings or invalid")
-        }
-        */
-
-        /**
-        lifecycleScope.launchWhenResumed {
-            signUpWithEmailViewModel.registerResult.collectLatest { result ->
-                when (result) {
-                    is Resource.Success -> {
-                        displaySnackBar("Account Registration Successful.")
-                        navigateToEmailVerificationFragment()
-                    }
-
-                    is Resource.Error -> {
-                        displaySnackBar("Could not register Account!")
-                    }
-                    is Resource.Loading -> {
-                        toast("Loading...")
-                    }
-                    null -> {}
-
-                }
-            }
-        }
-        */
-
-
+    private fun subscribeToObservers() {
+        viewModel.registerStatus.observe(viewLifecycleOwner, EventObserver(
+            onError = {
+                binding.progressBarSignUp.isVisible = false
+                snackBar(it)
+            },
+            onLoading = { binding.progressBarSignUp.isVisible = true }
+        ) {
+            binding.progressBarSignUp.isVisible = false
+            snackBar("Account created successfully")
+        })
     }
 
     private fun navigateToLogin() {
@@ -168,26 +104,7 @@ class SignUpDetailsWithEmailFragment : Fragment() {
         findNavController().navigate(R.id.action_signUpDetailsWithEmailFragment_to_emailVerificationFragment)
     }
 
-    private fun registerUser() {
-        signUpWithEmailViewModel.registerUsingEmail(
-            binding.inputUserName.text.toString(),
-            binding.inputEmailAddress.text.toString(),
-            binding.inputPassword.text.toString(),
-            binding.inputConfirmPassword.text.toString()
-        )
-    }
 
-    private fun isValidRegistrationDetails(): Boolean {
-        return binding.inputUserName.text.isNullOrEmpty().not().also {
-            if (!it) binding.enterUserName.error = "Invalid email"
-        } && binding.inputEmailAddress.text.isNullOrEmpty().not().also {
-            if (!it) binding.enterEmailAddress.error = "Invalid Name"
-        } && binding.inputPassword.text.isNullOrEmpty().not().also {
-            if (!it) binding.enterPassword.error = "Invalid Password"
-        } && binding.inputConfirmPassword.text.isNullOrEmpty().not().also {
-            if (!it) binding.confirmPassword.error = "Invalid Password"
-        }
-    }
     private fun navigateToPhoneFragment() {
         displaySnackBar("Feature coming soon...")
     }
