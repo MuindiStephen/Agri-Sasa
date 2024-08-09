8412a09256191e4b14be1fa0dc546f0292ba0431
MuindiStephen <stephenmuindi241@gmail.com>
2024-08-09 16:31:19 +0300

    code cleanup

---
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/MapsActivity.kt
@@ -0,0 +1,21 @@
+package com.steve_md.smartmkulima.ui.fragments.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.steve_md.smartmkulima.R
+
+class MapsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_maps)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
