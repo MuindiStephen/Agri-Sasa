d6d0784f5821f93c0a0105e467b6f99f03c7bed4
MuindiStephen <stephenmuindi241@gmail.com>
2024-09-05 15:16:24 +0300

    Setting up Pie Chart.

--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/others/crop_cycle/ViewFarmFinancialRecordsSummaryInDetailsFragment.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/others/crop_cycle/ViewFarmFinancialRecordsSummaryInDetailsFragment.kt
@@ -2,6 +2,7 @@ package com.steve_md.smartmkulima.ui.fragments.others.crop_cycle
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,12 +15,15 @@ import com.steve_md.smartmkulima.adapter.others.LocalFarmCycleAdapter
 import com.steve_md.smartmkulima.databinding.FragmentViewFarmFinancialRecordsSummaryInDetailsBinding
 import com.steve_md.smartmkulima.ui.activities.DetailedFarmCycleActivity
 import dagger.hilt.android.AndroidEntryPoint
+import ir.mahozad.android.PieChart
+import ir.mahozad.android.unit.Dimension
 import timber.log.Timber
 
 @AndroidEntryPoint
 class ViewFarmFinancialRecordsSummaryInDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentViewFarmFinancialRecordsSummaryInDetailsBinding
+    var revenue: Double? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,13 +38,43 @@ class ViewFarmFinancialRecordsSummaryInDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setUpUi()
+
+        setUpChart()
+    }
+
+    private fun setUpChart() {
+
+        val revenueFraction = revenue?.div((revenue!! + binding.textView129.text.toString().toDouble()))
+        val expensesFraction =  binding.textView129.text.toString().toDouble().div((revenue!! + binding.textView129.text.toString().toDouble()))
+
+        val pieChartSummaryFarmRecords = view?.findViewById<PieChart>(R.id.pieChartSummaryFarmRecords)
+        pieChartSummaryFarmRecords?.slices = listOf(
+              PieChart.Slice(revenueFraction!!.toFloat(), Color.rgb(120, 181, 0), Color.rgb(149, 224, 0), legend = "Total Expenses/Costs"),
+              PieChart.Slice(expensesFraction.toFloat(), Color.rgb(204, 168, 0), Color.rgb(249, 228, 0), legend = "Total Revenues"),
+            )
+
+        pieChartSummaryFarmRecords?.gradientType = PieChart.GradientType.RADIAL
+        pieChartSummaryFarmRecords?.legendIconsMargin = Dimension.DP(8F)
+        pieChartSummaryFarmRecords?.legendTitleMargin = Dimension.DP(14F)
+        pieChartSummaryFarmRecords?.legendLinesMargin = Dimension.DP(10F)
+        pieChartSummaryFarmRecords?.legendsMargin = Dimension.DP(20F)
+        pieChartSummaryFarmRecords?.legendsPercentageMargin = Dimension.DP(8F)
+        pieChartSummaryFarmRecords?.legendsSize = Dimension.DP(11F)
+        pieChartSummaryFarmRecords?.legendsPercentageSize = Dimension.DP(11F)
+        pieChartSummaryFarmRecords?.legendsIcon = PieChart.DefaultIcons.SQUARE
+        pieChartSummaryFarmRecords?.legendsTitle = "Crop Cycle Farm Records Summary"
+        pieChartSummaryFarmRecords?.legendPosition = PieChart.LegendPosition.BOTTOM
+        pieChartSummaryFarmRecords?.labelType = PieChart.LabelType.NONE
+        pieChartSummaryFarmRecords?.isLegendEnabled = true
+        pieChartSummaryFarmRecords?.legendsTitleColor = Color.BLACK
+        pieChartSummaryFarmRecords?.legendsColor = Color.BLACK
+
     }
 
     @SuppressLint("SetTextI18n")
     private fun setUpUi() {
 
         binding.textView126.text = arguments?.getString("cropCycleName")
-
         binding.textView129.text = arguments?.getString("totalInputs")
         binding.textView132.text = arguments?.getString("totalSales")
 
@@ -48,7 +82,7 @@ class ViewFarmFinancialRecordsSummaryInDetailsFragment : Fragment() {
         val expenses = binding.textView129.text.toString().toDouble()
         val sales = binding.textView132.text.toString().toDouble()
 
-        val revenue = binding.textView132.text.toString().toDouble() - binding.textView129.text.toString().toDouble()
+         revenue = binding.textView132.text.toString().toDouble() - binding.textView129.text.toString().toDouble()
 
                 // if sales > expenses
         if (binding.textView132.text.toString().toDouble() > binding.textView129.text.toString().toDouble()) {
