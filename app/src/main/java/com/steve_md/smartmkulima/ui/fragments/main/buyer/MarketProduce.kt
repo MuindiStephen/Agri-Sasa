9d01e22d5e224fb5dceb6b38c281c6d8e61418c1
MuindiStephen <stephenmuindi241@gmail.com>
2024-10-15 10:16:25 +0300

    fix: click to view order

--- app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/buyer/MarketProduce.kt
+++ app/src/main/java/com/steve_md/smartmkulima/ui/fragments/main/buyer/MarketProduce.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.steve_md.smartmkulima.R
 import com.steve_md.smartmkulima.adapter.FarmProduceAdapter
 import com.steve_md.smartmkulima.databinding.FragmentMarketProduceBinding
@@ -176,5 +177,10 @@ class MarketProduce : Fragment() {
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.farmProduceRecyclerView.isVisible = true
     }
 }
